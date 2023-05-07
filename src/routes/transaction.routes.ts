b2e5460ae98c9f74ee37dc19955b24aaae290d04
Alexandre <alexandre_c_g@hotmail.com>
2023-05-07 15:44:20 -0300

    chore: Chalenge finished

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,25 +1,53 @@
 import { Router } from 'express';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
 
 const transactionRouter = Router();
 
-// const transactionsRepository = new TransactionsRepository();
+const transactionsRepository = new TransactionsRepository();
 
 transactionRouter.get('/', (request, response) => {
   try {
-    // TODO
+    const transactions = transactionsRepository.all();
+    const balance = transactionsRepository.getBalance();
+
+    return response.status(200).json({
+      transactions,
+      balance,
+    });
+
   } catch (err) {
-    return response.status(400).json({ error: err.message });
+    if (err instanceof Error) {
+      return response.status(400).json({ error: err.message });
+    } else {
+      return response.status(400).json({ message:'Unexpected error', err })
+    }
   }
 });
 
 transactionRouter.post('/', (request, response) => {
   try {
-    // TODO
+    const { title, value, type } = request.body;
+
+    const createTransaction = new CreateTransactionService(
+      transactionsRepository
+    );
+
+    const transaction = createTransaction.execute({
+      title,
+      value,
+      type,
+    });
+
+    return response.status(200).json(transaction);
+
   } catch (err) {
-    return response.status(400).json({ error: err.message });
+    if (err instanceof Error) {
+      return response.status(400).json({ error: err.message });
+    } else {
+      return response.status(400).json({ message:'Unexpected error', err })
+    }
   }
 });
 
